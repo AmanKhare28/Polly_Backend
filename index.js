103483b5c5e04a9f358a55885da0355e60195bf4
Aman--Khare <aman22feb2004@gmail.com>
2025-01-14 23:02:53 +0530

    added a get method

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// https://polly-backend.onrender.com
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -7,6 +9,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send({ message: "hello from server" });
+});
 
 const PORT = process.env.PORT || 3000;
 
