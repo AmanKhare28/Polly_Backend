a5c25ed2e40277cc340dc3179c448175e8b1b057
Aman--Khare <aman22feb2004@gmail.com>
2025-01-14 23:14:55 +0530

    connected DB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -10,12 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send({ message: "hello from server" });
-});
+mongoose.connect(process.env.DB_URI);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on https://polly-backend.onrender.com`);
 });
