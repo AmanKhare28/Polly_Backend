694158c42a40c6fb88b92024eb29c1ffe3ca992a
Aman--Khare <aman22feb2004@gmail.com>
2025-01-20 11:23:19 +0530

    added active toggle functionality

--- routes/poll.route.js
+++ routes/poll.route.js
@@ -1,6 +1,8 @@
 import express from "express";
 import createPoll from "../controllers/poll.js";
+import togglePoll from "../controllers/togglePoll.js";
 
 const router = express.Router();
 router.post("/new", createPoll);
+router.post("/toggle", togglePoll);
 export default router;
