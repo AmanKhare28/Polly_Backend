9ecfc2048b897435fa48335b20b1ed27dd5abcec
Aman--Khare <aman22feb2004@gmail.com>
2025-01-20 11:40:39 +0530

    resolved toggle error

--- controllers/togglePoll.js
+++ controllers/togglePoll.js
@@ -4,13 +4,14 @@ const togglePoll = async (req, res) => {
   const { pollId } = req.body;
   try {
     const poll = await Poll.findById(pollId);
-    if (!poll) {
-      return res.status(404).json({ message: "Poll not found" });
-    }
+    // if (!poll) {
+    //   return res.status(404).json({ message: "Poll not found" });
+    // }
     poll.active = !poll.active;
     await poll.save();
     res.status(200).json({ message: "toggle successful" });
   } catch (err) {
+    console.log(err);
     return res
       .status(500)
       .json({ message: "internal server error", error: err || err.message });
