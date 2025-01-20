b9d488a3f4620eed35fa7425ce58648c7b187fd3
Aman--Khare <aman22feb2004@gmail.com>
2025-01-20 12:33:04 +0530

    get request for all the polls created by a user

--- routes/poll.route.js
+++ routes/poll.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import createPoll from "../controllers/poll.js";
 import togglePoll from "../controllers/togglePoll.js";
+import getPolls from "../controllers/getPolls.js";
 
 const router = express.Router();
 router.post("/new", createPoll);
 router.post("/toggle", togglePoll);
+router.get("/fetchall", getPolls);
 export default router;
