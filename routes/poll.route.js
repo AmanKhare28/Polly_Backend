ce3d304bbf58c781677e4cac79369f602d9b1e4a
Aman--Khare <aman22feb2004@gmail.com>
2025-01-24 10:21:14 +0530

    poll detail fetch

--- routes/poll.route.js
+++ routes/poll.route.js
@@ -3,10 +3,12 @@ import createPoll from "../controllers/poll.js";
 import togglePoll from "../controllers/togglePoll.js";
 import getPolls from "../controllers/getPolls.js";
 import deletePoll from "../controllers/deletePoll.js";
+import getPoll from "../controllers/getPoll.js";
 
 const router = express.Router();
 router.post("/new", createPoll);
 router.post("/toggle", togglePoll);
 router.post("/fetchall", getPolls);
 router.delete("/delete", deletePoll);
+router.get("/fetch", getPoll);
 export default router;
