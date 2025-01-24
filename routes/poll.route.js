61269f100d764166dabc316655b5b36158086f16
Aman--Khare <aman22feb2004@gmail.com>
2025-01-24 09:48:41 +0530

    delete poll functionality

--- routes/poll.route.js
+++ routes/poll.route.js
@@ -2,9 +2,11 @@ import express from "express";
 import createPoll from "../controllers/poll.js";
 import togglePoll from "../controllers/togglePoll.js";
 import getPolls from "../controllers/getPolls.js";
+import deletePoll from "../controllers/deletePoll.js";
 
 const router = express.Router();
 router.post("/new", createPoll);
 router.post("/toggle", togglePoll);
 router.post("/fetchall", getPolls);
+router.delete("/delete", deletePoll);
 export default router;
