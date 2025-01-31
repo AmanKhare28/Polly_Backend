c2e98b2bc9b7559b26af41961d6fc3032cdc6668
Aman--Khare <aman22feb2004@gmail.com>
2025-01-31 11:30:31 +0530

    added activivty check

--- routes/poll.route.js
+++ routes/poll.route.js
@@ -5,6 +5,7 @@ import getPolls from "../controllers/getPolls.js";
 import deletePoll from "../controllers/deletePoll.js";
 import getPoll from "../controllers/getPoll.js";
 import Vote from "../controllers/vote.js";
+import Check from "../controllers/checkActivity.js";
 
 const router = express.Router();
 router.post("/new", createPoll);
@@ -13,4 +14,5 @@ router.post("/fetchall", getPolls);
 router.delete("/delete", deletePoll);
 router.post("/fetch", getPoll);
 router.post("/vote", Vote);
+router.post("/check", Check);
 export default router;
